5933352aad215c1ead1b8c910af141e30fd2f879
Olivers11 <olstertecn597@gmail.com>
2022-03-22 16:45:27 -0600

    Changes from package.json saved

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
+import { MongooseModule } from '@nestjs/mongoose';
+
+const url = "mongodb://localhost/products";
 
 @Module({
-  imports: [],
+  imports: [ProductModule, MongooseModule.forRoot(url)],
   controllers: [AppController],
   providers: [AppService],
 })
